3a123567cb1b85b84ded66263ed8909faf1d0d0a
kalibetre <kalbetre@icloud.com>
2023-04-20 10:52:42 +0300

    spring project scaffolding

---
+++ src/main/java/com/geselaapi/GeselaApiApplication.java
@@ -0,0 +1,13 @@
+package com.geselaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeselaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeselaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geselaapi/GeselaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geselaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeselaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
