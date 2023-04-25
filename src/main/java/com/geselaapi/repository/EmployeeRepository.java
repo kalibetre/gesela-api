8a31d5bce795f9d359106ab40376cfef00679e3e
kalibetre <kalbetre@icloud.com>
2023-04-25 13:21:21 +0300

    move findByEmail to user repository

--- src/main/java/com/geselaapi/repository/EmployeeRepository.java
+++ src/main/java/com/geselaapi/repository/EmployeeRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
-    Optional<Employee> findByEmail(String email);
     Optional<Employee> findByUserAccount(User userAccount);
 }
--- src/main/java/com/geselaapi/repository/UserRepository.java
+++ src/main/java/com/geselaapi/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package com.geselaapi.repository;
 
-import com.geselaapi.model.Customer;
 import com.geselaapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface CustomerRepository extends JpaRepository<Customer, UUID> {
-    Optional<Customer> findByUserAccount(User userAccount);
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
