6d448eabc4b27a8c6167b36e9a75c81ddd8fc66f
kalibetre <kalbetre@icloud.com>
2023-04-29 10:24:04 +0300

    add security scheme for swagger

--- src/main/java/com/geselaapi/GeselaApiApplication.java
+++ src/main/java/com/geselaapi/GeselaApiApplication.java
@@ -1,9 +1,13 @@
 package com.geselaapi;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(name = "gesela-api", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class GeselaApiApplication {
 
 	public static void main(String[] args) {
