8b4e1290492a53eafc60c1f13edff8170c1bd2aa
kalibetre <kalbetre@icloud.com>
2023-04-29 10:21:49 +0300

    add Jwt filter and service

---
+++ src/main/java/com/geselaapi/service/JwtService.java
@@ -0,0 +1,66 @@
+package com.geselaapi.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.UUID;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    @Value("#{ '${SECRETE_KEY}' }")
+    private String SECRETE_KEY;
+
+    public String extractUserUuid(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UUID userUuid) {
+        return Jwts
+                .builder()
+                .setSubject(String.valueOf(userUuid))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UUID userUuid) {
+        final String username = extractUserUuid(token);
+        return username.equals(String.valueOf(userUuid)) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRETE_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
