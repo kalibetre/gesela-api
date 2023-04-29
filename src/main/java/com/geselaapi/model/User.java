8bd6726f225e470b0b56d23d403b5feac69b9a26
kalibetre <kalbetre@icloud.com>
2023-04-29 10:11:05 +0300

    update user model to implement UserDetails

--- src/main/java/com/geselaapi/model/User.java
+++ src/main/java/com/geselaapi/model/User.java
@@ -1,13 +1,17 @@
 package com.geselaapi.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
-public class User extends BaseModel {
+public class User extends BaseModel implements UserDetails {
 	@Column(nullable = false)
 	private String name;
 	@Column(nullable = false, unique = true)
@@ -16,7 +20,11 @@ public class User extends BaseModel {
 	private String phone;
 	@Column(nullable = false)
 	@JsonIgnore
-	private String passwordHash;
+	private String password;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private UserRole role;
 
 	public String getName() {
 		return name;
@@ -42,11 +50,50 @@ public class User extends BaseModel {
 		this.phone = phone;
 	}
 
-	public String getPasswordHash() {
-		return passwordHash;
+	@Override
+	public String getPassword() {
+		return password;
 	}
 
-	public void setPasswordHash(String passwordHash) {
-		this.passwordHash = passwordHash;
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserRole getRole() {
+		return role;
+	}
+
+	public void setRole(UserRole role) {
+		this.role = role;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(role.name()));
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 }
