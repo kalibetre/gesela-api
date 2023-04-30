47b564ba8b84cf92b8b2c3934025b68e12e4bb67
kalibetre <kalbetre@icloud.com>
2023-04-30 12:17:46 +0300

    rename methods to reflect attributes

--- src/main/java/com/geselaapi/repository/IssueRepository.java
+++ src/main/java/com/geselaapi/repository/IssueRepository.java
@@ -1,5 +1,6 @@
 package com.geselaapi.repository;
 
+import com.geselaapi.model.Employee;
 import com.geselaapi.model.Issue;
 import com.geselaapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,6 @@ import java.util.List;
 import java.util.UUID;
 
 public interface IssueRepository extends JpaRepository<Issue, UUID> {
-    List<Issue> findByRaisedBy(User raisedBy);
-    List<Issue> findByHandler(User handler);
+    List<Issue> findByUser(User raisedBy);
+    List<Issue> findByHandler(Employee handler);
 }
