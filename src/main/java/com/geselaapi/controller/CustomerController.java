5409433b3fb4b67cc9f83c882157f6e03f2d8953
kalibetre <kalbetre@icloud.com>
2023-05-07 12:33:20 +0300

    add CustomerController

---
+++ src/main/java/com/geselaapi/controller/CustomerController.java
@@ -0,0 +1,110 @@
+package com.geselaapi.controller;
+
+import com.geselaapi.dto.CustomerResponseDTO;
+import com.geselaapi.dto.CustomerUpdateDTO;
+import com.geselaapi.model.AccountStatus;
+import com.geselaapi.model.Customer;
+import com.geselaapi.model.User;
+import com.geselaapi.model.UserRole;
+import com.geselaapi.repository.CustomerRepository;
+import com.geselaapi.service.UserService;
+import com.geselaapi.utils.Converter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/customers")
+@SecurityRequirement(name = "gesela-api")
+public class CustomerController {
+    private final CustomerRepository customerRepository;
+    private final UserService userService;
+
+    public CustomerController(CustomerRepository customerRepository, UserService userService) {
+        this.customerRepository = customerRepository;
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<?> getCustomers(@PathVariable UUID id) {
+        User user = userService.getAuthenticatedUser();
+        if (user != null && user.getRole() != UserRole.DEFAULT) {
+            List<Customer> customers = customerRepository.findAll();
+            return ResponseEntity.ok(Converter.convertList(customers, CustomerResponseDTO::from));
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCustomer(@PathVariable UUID id) {
+        User user = userService.getAuthenticatedUser();
+        if (user != null && (user.getUuid() == id || user.getRole() != UserRole.DEFAULT)) {
+            Customer customer = customerRepository.findById(id).orElse(null);
+            if (customer == null)
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer with the specified id not found");
+            return ResponseEntity.ok(CustomerResponseDTO.from(customer));
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateCustomer(@PathVariable UUID id, @Valid @RequestBody CustomerUpdateDTO customer) {
+        User user = userService.getAuthenticatedUser();
+        if (user != null && (user.getUuid() == id || user.getRole() == UserRole.ADMIN)) {
+            Customer existingCustomer = customerRepository.findById(id).orElse(null);
+            if (existingCustomer == null)
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer with the specified id not found");
+
+            if (customer.getAccountStatus() != null)
+                existingCustomer.setAccountStatus(customer.getAccountStatus());
+
+            customerRepository.save(existingCustomer);
+            return ResponseEntity.ok(CustomerResponseDTO.from(existingCustomer));
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @PutMapping("/{id}/block")
+    public ResponseEntity<?> blockCustomer(@PathVariable UUID id) {
+        User user = userService.getAuthenticatedUser();
+        if (user != null && (user.getUuid() == id || user.getRole() == UserRole.ADMIN)) {
+            Customer customer = customerRepository.findById(id).orElse(null);
+            if (customer == null)
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer with the specified id not found");
+
+            if (customer.getAccountStatus() == AccountStatus.ARCHIVED)
+                return ResponseEntity.badRequest().body("Customer is archived");
+
+            customer.setAccountStatus(AccountStatus.BLOCKED);
+            customerRepository.save(customer);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCustomer(@PathVariable UUID id) {
+        User user = userService.getAuthenticatedUser();
+        if (user != null && (user.getUuid() == id || user.getRole() == UserRole.ADMIN)) {
+            Customer customer = customerRepository.findById(id).orElse(null);
+            if (customer == null)
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer with the specified id not found");
+
+            if (customer.getAccountStatus() != AccountStatus.ARCHIVED) {
+                try {
+                    customerRepository.delete(customer);
+                } catch (Exception e) {
+                    customer.setAccountStatus(AccountStatus.ARCHIVED);
+                    customerRepository.save(customer);
+                }
+            }
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+}
