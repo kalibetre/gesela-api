99054afd5d3040086cec654adf6775b787474b18
kalibetre <kalbetre@icloud.com>
2023-05-15 00:55:26 +0300

    add uuid to department response dto

--- src/main/java/com/geselaapi/dto/DepartmentResponseDTO.java
+++ src/main/java/com/geselaapi/dto/DepartmentResponseDTO.java
@@ -5,8 +5,10 @@ import com.geselaapi.utils.Converter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class DepartmentResponseDTO {
+    private UUID uuid;
     private String name;
     private String description;
 
@@ -14,11 +16,20 @@ public class DepartmentResponseDTO {
 
     public static DepartmentResponseDTO from(Department department) {
         DepartmentResponseDTO responseDTO = new DepartmentResponseDTO();
+        responseDTO.setUuid(department.getUuid());
         responseDTO.setName(department.getName());
         responseDTO.setDescription(department.getDescription());
         List<EmployeeResponseDTO> employees = Converter.convertList(department.getEmployees(), EmployeeResponseDTO::from);
         employees.forEach(employee -> responseDTO.getEmployees().add(employee));
         return responseDTO;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
     }
 
     public String getName() {
