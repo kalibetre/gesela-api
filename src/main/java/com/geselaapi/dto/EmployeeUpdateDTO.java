b65d0cff81d731ec3de8d2b3ed53faf317dba6e5
kalibetre <kalbetre@icloud.com>
2023-05-16 00:55:46 +0300

    refactor employee dto to employee update dto

--- src/main/java/com/geselaapi/dto/EmployeeUpdateDTO.java
+++ src/main/java/com/geselaapi/dto/EmployeeUpdateDTO.java
@@ -1,25 +1,53 @@
 package com.geselaapi.dto;
 
-import java.time.LocalDate;
+import com.geselaapi.model.UserRole;
+
+import java.util.UUID;
 
 public class EmployeeUpdateDTO {
-    private String department;
-    private LocalDate hireDate;
+    private String name;
+    private String email;
+    private String phone;
+    private UUID departmentUuid;
+    private UserRole role;
 
-    public String getDepartment() {
-        return department;
+    public String getName() {
+        return name;
     }
 
-    public void setDepartment(String department) {
-        this.department = department;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public LocalDate getHireDate() {
-        return hireDate;
+    public String getEmail() {
+        return email;
     }
 
-    public void setHireDate(LocalDate hireDate) {
-        this.hireDate = hireDate;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public UUID getDepartmentUuid() {
+        return departmentUuid;
+    }
+
+    public void setDepartmentUuid(UUID departmentUuid) {
+        this.departmentUuid = departmentUuid;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
 }
