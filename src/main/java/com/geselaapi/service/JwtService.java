1d5a23b6c8303d4888ae01322a02f26c54f3eb39
kalibetre <kalbetre@icloud.com>
2023-05-17 20:19:55 +0300

    set expiration to 24hrs

--- src/main/java/com/geselaapi/service/JwtService.java
+++ src/main/java/com/geselaapi/service/JwtService.java
@@ -32,7 +32,7 @@ public class JwtService {
                 .builder()
                 .setSubject(String.valueOf(userUuid))
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
