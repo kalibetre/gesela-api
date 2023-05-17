918c468a5c36fdcd815ebdf503c09f216305b337
kalibetre <kalbetre@icloud.com>
2023-05-17 20:19:39 +0300

    add find by phone method definition

--- src/main/java/com/geselaapi/repository/UserRepository.java
+++ src/main/java/com/geselaapi/repository/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByPhone(String phone);
 }
