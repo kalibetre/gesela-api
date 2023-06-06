597b2ee0ff39370feb7204ae822a4468f1cb53df
kalibetre <kalbetre@icloud.com>
2023-06-06 21:48:11 +0300

    add seen and uuid property

--- src/main/java/com/geselaapi/dto/NotificationsDTO.java
+++ src/main/java/com/geselaapi/dto/NotificationsDTO.java
@@ -3,20 +3,33 @@ package com.geselaapi.dto;
 import com.geselaapi.model.Notification;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class NotificationsDTO {
+    private UUID uuid;
     private String message;
     private UserResponseDTO fromUser;
     private UserResponseDTO toUser;
     private LocalDateTime date;
+    private boolean seen;
 
     public static NotificationsDTO from(Notification notification) {
         NotificationsDTO notificationsDTO = new NotificationsDTO();
+        notificationsDTO.setUuid(notification.getUuid());
         notificationsDTO.setMessage(notification.getMessage());
         notificationsDTO.setFromUser(UserResponseDTO.from(notification.getFromUser()));
         notificationsDTO.setToUser(UserResponseDTO.from(notification.getToUser()));
-        notificationsDTO.date = notification.getTimeStamp();
+        notificationsDTO.setDate(notification.getTimeStamp());
+        notificationsDTO.setSeen(notification.isSeen());
         return notificationsDTO;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
     }
 
     public String getMessage() {
@@ -49,5 +62,13 @@ public class NotificationsDTO {
 
     public void setDate(LocalDateTime date) {
         this.date = date;
+    }
+
+    public boolean isSeen() {
+        return seen;
+    }
+
+    public void setSeen(boolean seen) {
+        this.seen = seen;
     }
 }
